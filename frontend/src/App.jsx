2c84b571bae4a9d4fd53dea430ca77a7ad7e368c
unknown <z5324093@ad.unsw.edu.au>
2022-11-14 01:26:14 +1100

    random

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './screens/landingPage';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
 function App () {
-  return <></>;
+  // because of the need to use react router, everything is wrapped in landingPage.
+  return (
+    <Router>
+      <LandingPage />
+    </Router>
+  );
 }
 
 export default App;
